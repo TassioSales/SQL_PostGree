944023ba909682c702254615868d142ec09404f2
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-12-22 19:56:12 -0300

    inserindo dados na tabela

---
+++ inserindo_dados_na_tabela.sql
@@ -0,0 +1,51 @@
+--Consultando tabela
+
+select * from products;
+
+--Inserindo dados na tabela;
+
+insert into products (id, name, description, category, price, in_stock)
+values (1, 'Computador', 'Computador 8 nucleos', 'Informatica', 5500, 't');
+
+insert into products (id, name, description, category, price, in_stock)
+values (2, 'Celular', 'Dual Chip', 'Eletronicos', 1800, 't');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (3, 'Ventilador', 'Ventilador 4 velocidades', 'Eletrodomestico', 180.90, 'f');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (4, 'Televisor', 'Televisor Smart 42 pol.', 'Eletroeletronico', 1750.60, 't');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (5, 'Mouse', 'Mouse sem fio', 'Informatica', 38, 't');
+insert into products (id, name, description, category, price, in_stock) 
+values (6, 'Bicicleta', 'Bicicleta Mtb Aro 29', 'Esporte', 1360, 't');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (7, 'Geladeira', 'Geladeira Duplex', 'Eletrodomestico', 2230.0, 't');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (8, 'Ar condicionado', 'Ar Condicionado Split 12000 Btus', 'Eletrodomestico', 1890.0, 'f');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (9, 'Raquete de tênis', 'Raquete de tênis Graphene 360+', 'Esporte', 670.0, 'f');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (10, 'Sofá', 'Sofá Retrátil e Reclinável', 'Moveis', 1245.0, 't');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (11, 'Cama box', 'Cama box conjugado casal', 'Moveis', 580.0, 'f');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (12, 'Mesa', 'Mesa de jantar c/ 4 cadeiras', 'Moveis', 480.0, 't');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (13, 'Microfone', 'Microfone condensador', 'Eletronico', 380.0, 't');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (14, 'Mesa de tênis', 'Mesa de tênis ping pong', 'Esporte', 723.0, 't');
+
+insert into products (id, name, description, category, price, in_stock) 
+values (15, 'Cafeteira', 'Cafeteira expresso automática', 'Eletrodomestico', 450.0, 't');
+
+ 
